5c561ba1883dffd3ceb9a15830e6771fb9a0e0a9
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-05-27 18:05:59 +0200

    feat: Setup complete portfolio structure with sections and components
    - Create main page with all required sections
      - hero, about, skills, projects, testimonials, contact
    - Implement shared layout components (header & footer)
    - Add reusable UI components in shared folder
      - skill-card, project-card, testimonial-card, custom-button
    - Create projects module structure with list and detail views
    - Configure initial routing

---
+++ src/app/shared/components/custom-buttom/custom-buttom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-buttom',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-buttom.component.html',
+  styleUrl: './custom-buttom.component.scss'
+})
+export class CustomButtomComponent {
+
+}
