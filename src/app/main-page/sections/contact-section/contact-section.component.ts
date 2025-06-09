e0d4dfee6be3b76370f8eef522d853855ac4a4fd
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-06-09 15:44:06 +0200

    feat: enhance contact form with dynamic placeholders and error styling

--- src/app/main-page/sections/contact-section/contact-section.component.ts
+++ src/app/main-page/sections/contact-section/contact-section.component.ts
@@ -7,7 +7,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './contact-section.component.html',
   styleUrls: ['./contact-section.component.scss']
