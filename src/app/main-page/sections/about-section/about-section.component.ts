560b53bd737664909d53d1d914bc27c31c6847cd
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-06-11 17:56:54 +0200

    refactor: replace custom button component with a standard button and update styles

--- src/app/main-page/sections/about-section/about-section.component.ts
+++ src/app/main-page/sections/about-section/about-section.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CustomButtonComponent } from '../../../shared/components/custom-button/custom-button.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-section',
   standalone: true,
-  imports: [CommonModule, CustomButtonComponent],
+  imports: [CommonModule],
   templateUrl: './about-section.component.html',
   styleUrl: './about-section.component.scss'
 })
