e8bb548bd09fcdad03415c1d91054a7bf11bbab4
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-09-02 13:57:05 +0200

    style: refactor translation service configuration for improved readability and structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,22 +2,26 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
-import {provideTranslateService, TranslateLoader} from "@ngx-translate/core";
+import {TranslateModule, TranslateLoader, TranslateService} from "@ngx-translate/core";
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient} from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
     new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     provideHttpClient(),
-    provideTranslateService({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
-      },
-    })
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    )
   ],
 };
